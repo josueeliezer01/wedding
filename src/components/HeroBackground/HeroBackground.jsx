38477fee8114c7748ce97a5fe559830a2512101f
josueeliezer01 <eliezercasstilho@gmail.com>
2026-01-08 18:15:09 +0000

    feat(hero): aesthetic refactoring

--- src/components/HeroBackground/HeroBackground.jsx
+++ src/components/HeroBackground/HeroBackground.jsx
@@ -8,6 +8,8 @@ export default function HeroBackground() {
       role="presentation">
       <div className="hero-bg__spot hero-bg__spot--left" />
       <div className="hero-bg__spot hero-bg__spot--right" />
+
+      <div className="hero-pattern" />
     </div>
   );
 }
